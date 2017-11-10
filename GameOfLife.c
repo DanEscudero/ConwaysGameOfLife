53cfbd3bb90688d508285c2420feb4500e29eb89
DanEscudero <daniel.escudero05@gmail.com>
2017-11-09 23:41:54 -0200

    1 warning missing

--- GameOfLife.c
+++ GameOfLife.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 3
-#define ROUNDS 7
+
+/* Starts a square matrix and returns the pointer to it */
+int ** startMatrix (int size)
+{
+	int ** matrix = malloc (sizeof (int *) * size);
+	for (int i = 0; i < size; i++)
+		matrix [i] = malloc (sizeof (int) * size);
+	return matrix;
+}
 
 /* Fills a Matrix with a given element x */
-void fillBoard (int M[TAM][TAM], int m, int n, int x)
+void fillBoard (int **M, int size, int x)
 {
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
 			M[i][j] = x;
 }
 
 /* Scan from keyboard elements to the Matrix */
-void readBoard (int M[TAM][TAM], int m, int n)
+void readBoard (int **M, int size)
 {
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
 			scanf ("%d", &M[i][j]);
 }
 
 /* Prints out a Matrix */
-void printBoard (int M[TAM][TAM], int m, int n)
+void printBoard (int **M, int size)
 {
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			printf ("%d   ", M[i][j]);
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			printf ("%d  ", M[i][j]);
 		}
 		printf ("\n");
 	}
 }
 
 /* Both following functions are called to make *
- * sutre not to iterate out of Matrix bounds   */
+ * sure not to iterate out of Matrix bounds    */
 int defStart (int x)
 {
 	if (!x) return 0;
@@ -44,16 +52,16 @@ int defEnd (int x, int max)
 	else return x+1;
 }
 
-/* Receives the Board, a position [i,j] and the matrix size (mxn) *
+/* Receives the Board, a position [i,j] and the matrix size (m*n) *
  * Returns the sum of the neighbours of the position 			  */
-int n_around (int Board[TAM][TAM], int i, int j, int m, int n)
+int n_around (int **Board, int i, int j, int size)
 {
 	int startLine, startCol, endLine, endCol, around = 0;
 	
 	startLine = defStart (i);
 	startCol = defStart (j);
-	endLine = defEnd (i, m);
-	endCol = defEnd (j, n);
+	endLine = defEnd (i, size);
+	endCol = defEnd (j, size);
 	
 	for (int k = startLine; k <= endLine; k++)
 		for (int l = startCol; l <= endCol; l++)
@@ -65,61 +73,55 @@ int n_around (int Board[TAM][TAM], int i, int j, int m, int n)
 
 /* Fills the matrix Around with the      *
  * number of neighbours of each position */
-void fillAround (int Around[TAM][TAM], int Board[TAM][TAM], int m, int n)
+void fillAround (int **Around, int **Board, int size)
 {
-	for (int i = 0; i < m; i ++)
-		for (int j = 0; j < n; j++)
-			Around[i][j] = n_around (Board, i, j, m, n);
+	for (int i = 0; i < size; i ++)
+		for (int j = 0; j < size; j++)
+			Around[i][j] = n_around (Board, i, j, size);
 }
 
-void fillNewBoard (int Board[TAM][TAM], int NewBoard[TAM][TAM], int Around [TAM][TAM], int m, int n)
+void fillNewBoard (int **Board, int **NewBoard, int **Around, int size)
 {
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
 			if (Around[i][j] == 3) NewBoard[i][j] = 1;
 			if (Around[i][j] == 2) NewBoard[i][j] = Board[i][j];
 		}
 	}
 }
 
-/* Refresh Board means swap  elements on Board with elements on NewBoard */
-void refreshBoard (int Board[TAM][TAM], int NewBoard[TAM][TAM], int m, int n)
+/* Refresh Board means swap elements on Board with elements on NewBoard */
+void refreshBoard (int ***Board, int ***NewBoard, int size)
 {
-	int aux;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			aux = Board[i][j];
-			Board[i][j] = NewBoard[i][j];
-			NewBoard[i][j] = aux;
-		}
-	}
-}
+	//Working, even with warning
+	int ***aux;
+	aux = *Board;
+	*Board = *NewBoard;
+	*NewBoard = aux;
 
-/* Cleans and Fills NewBoard, refreshes Board and repeat */
-void playGame (int rounds, int Board[TAM][TAM], int NewBoard[TAM][TAM], int Around[TAM][TAM], int m, int n)
-{
-	for (int i = 2; i <= rounds+1; i++) {
-		//Lock to better looking
-		for (int k = 0; k < 1000000000; k++);
-		fillBoard (NewBoard, m, n, 0);
-		fillAround (Around, Board, m, n);
-		fillNewBoard (Board, NewBoard, Around, m, n);
-		printf ("\n=== GEN %d ===\n", i);
-		printBoard (NewBoard, m, n);
-		refreshBoard (Board, NewBoard, m, n);
-	}
 }
 
 int main ()
 {
-	int Board [TAM][TAM];
-	int Around [TAM][TAM];
-	int NewBoard [TAM][TAM];
+	int **Board = startMatrix (TAM);
+	int **Around = startMatrix (TAM);
+	int **NewBoard = startMatrix (TAM);
+		
+	readBoard (Board, TAM);
 	
-	readBoard (Board, TAM, TAM);
-	printf ("=== INICIO ===\n");
-	printBoard (Board, TAM, TAM);
-	playGame (ROUNDS, Board, NewBoard, Around, TAM, TAM);
+	printf ("\n=== GEN 1 ===\n");
+	printBoard (Board, TAM);
+	
+	for (int i = 2; ; i++) {
+		for (int k = 0; k < 200000000; k++);			//Lock to better view
+		fillBoard (NewBoard, TAM, 0);
+		fillAround (Around, Board, TAM);
+		fillNewBoard (Board, NewBoard, Around, TAM);
+		system ("clear");
+		printf ("\n=== GEN %d ===\n", i);
+		printBoard (NewBoard, TAM);
+		refreshBoard (&Board, &NewBoard, TAM);
+	}
 	
 	return 0;
 }
